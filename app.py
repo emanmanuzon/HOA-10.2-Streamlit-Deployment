2da9ee1f8e13d091332b4e4c13619a64c2500da8
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 02:09:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,3 @@ file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(100,100)
-    image=ImageOps.fit(image_data,size)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
