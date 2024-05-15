6c640de9512e014fa60d5f2fce6925f5fd47be6d
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-16 02:10:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
-def load_model():
-  model=tf.keras.models.load_model('CNN_Model_7.h5')
-  return model
-model=load_model()
 st.write("""
-# Weather Detection System"""
+# Mood Detection"""
 )
-file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
